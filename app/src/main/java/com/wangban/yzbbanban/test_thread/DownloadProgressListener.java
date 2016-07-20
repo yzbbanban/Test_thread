efc59528dad3a6691594e4b77ecd1edebe5703d7
yzbbanban <yzbbanban@live.com>
2016-07-20 21:02:05 +0800

    多线程断点下载 Demo

---
+++ app/src/main/java/com/wangban/yzbbanban/test_thread/DownloadProgressListener.java
@@ -0,0 +1,13 @@
+package com.wangban.yzbbanban.test_thread;
+
+/**
+ * Created by YZBbanban on 16/7/20.
+ */
+public interface DownloadProgressListener {
+    /**
+     * 下载进度监听方法 获取和处理下载点数据的大小
+     *
+     * @param size 数据大小
+     */
+    void onDownloadSize(int size);
+}
